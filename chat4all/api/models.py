64bac3f990e78ebcbe28618ccb4195cafd4b5bc6
Javier Felipe Feo Cortés <jfelipefc@gmail.com>
2020-03-07 22:21:14 -0500

    (db) user and contact models arrangement
    The 'email' field for the 'User' will have an unique constraint.
    Alongside this change, the 'Contacts' model will contain an
    'unique_together' constraint in its two fields.

--- chat4all/api/models.py
+++ chat4all/api/models.py
@@ -11,7 +11,7 @@ class User(AbstractBaseUser):
     Class attributes:
         id (models.UUIDField): UUID (v4) used to indentify the user
         first_name (models.CharField): User first name
-        first_name (models.CharField): User last name
+        last_name (models.CharField): User last name
         gender (models.CharField): User gender
         email (models.EmailField): User email. This field will be used for
         profile_picture (models.CharField): Path to the user profile picture
@@ -24,8 +24,8 @@ class User(AbstractBaseUser):
     first_name = models.CharField(max_length=256)
     last_name = models.CharField(max_length=256)
     gender = models.CharField(max_length=256)
-    email = models.EmailField()
-    profile_picture = models.CharField(max_length=256)
+    email = models.EmailField(unique=True)
+    profile_picture = models.CharField(max_length=256, null=True)
     created_at = models.DateTimeField(default=timezone.now)
     updated_at = models.DateTimeField(default=timezone.now)
 
@@ -34,14 +34,17 @@ class User(AbstractBaseUser):
 
 class Contact(models.Model):
     """
-    Defines a user contact
+    Defines a contact
 
     Class attributes:
-        user (models.ForeignKey): User id
+        user (models.ForeignKey): related user id
         contact_id (models.ForeignKey) Contact id
     """
 
     user = models.ForeignKey(User, on_delete=models.CASCADE,
-                             related_name='user_contacts')
+                             related_name='related_user')
     contact = models.ForeignKey(User, on_delete=models.CASCADE,
-                                related_name='contacts_user')
+                                related_name='contacts')
+
+    class Meta:
+        unique_together = ('user', 'contact')
