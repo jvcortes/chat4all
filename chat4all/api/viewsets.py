faf08ce2752dcae6a71308911c922bd9a3da1edb
Javier Felipe Feo Cortés <jfelipefc@gmail.com>
2020-03-09 10:44:47 -0500

    (feat) optional param for '/contacts/' endpoint
    Implemented optional parameter 'contact_id' for the
    '/users/<user_id>/contacts' API endpoint, for retrieving a single
    contact.

--- chat4all/api/viewsets.py
+++ chat4all/api/viewsets.py
@@ -1,4 +1,4 @@
-import json
+import uuid
 from rest_framework import viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
@@ -12,15 +12,21 @@ class UserViewSet(viewsets.ModelViewSet):
 
     @action(methods=['get'],
             detail=True,
-            url_path='contacts',
+            url_path='contacts(?:/(?P<contact_id>[0-9a-f-]+))?',
             url_name='list-contacts')
-    def list_contacts(self, request, pk=None):
-        queryset = models.User.objects.get(pk=pk).get_contacts()
-        serializer = serializers.UserSerializer(queryset,
-                                                context={'request':
-                                                         request},
-                                                many=True)
-        return Response(serializer.data)
+    def list_contacts(self, request, pk=None, contact_id=None):
+        user = models.User.objects.get(pk=pk)
+        if contact_id:
+            queryset = list(filter(lambda contact:
+                                   contact.id == uuid.UUID(contact_id),
+                                   user.get_contacts()))
+        else:
+            queryset = user.get_contacts()
+
+        return Response(
+            serializers.UserSerializer(queryset,
+                                       context={'request': request},
+                                       many=True).data)
 
 
 class ContactViewSet(viewsets.ModelViewSet):
